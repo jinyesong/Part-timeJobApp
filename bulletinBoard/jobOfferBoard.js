f2fb870454b35c403743f611d6708a01e83f3fae
GwakMirae <alfo9490@naver.com>
2021-11-10 19:47:08 +0900

    feat: reset button

--- bulletinBoard/jobOfferBoard.js
+++ bulletinBoard/jobOfferBoard.js
@@ -5,3 +5,14 @@ document.getElementById("filter").onclick = function(){
 document.getElementById("confirm").onclick = function(){
     document.getElementById("modal").style.display="none";
 }
+
+document.getElementById("reset").onclick = function(){
+    document.getElementsByName("gender")[0].checked=true;
+    document.getElementById("area").options[0].selected=true;
+    document.getElementById("period").options[0].selected=true;
+    document.getElementById("pay").value = "최저시급";
+}
+
+document.getElementById("write").onclick = function(){
+    location.href="./"
+}
