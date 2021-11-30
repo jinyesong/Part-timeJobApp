6b2b128b66ec2278633a5c5d07e17f52d0d3a1d6
GwakMirae <alfo9490@naver.com>
2021-11-30 16:10:15 +0900

    add register connect with firestore

--- public/signupPage/signup.js
+++ public/signupPage/signup.js
@@ -16,20 +16,106 @@ const db = firebase.firestore();
     })
   });
 
-// import { doc, updateDoc } from "firebase/firestore";
+function isName(){
+  if($('#userName').val() == ""){
+    alert("이름을 입력해주세요");
+    return false;
+  }
+  return true;
+}
 
-// var name = document.getElementsByName("userName").value
-// var email = document.getElementsByName("userId").value
-// var pw = document.getElementsByName("userPW").value
-// var pw2 = document.getElementsByName("userPW2").value
-// var addr = document.getElementsByName("userAddr").value
-// var birth = document.getElementsByName("userBirthday").value
-// var phoneNum = document.getElementsByName("userphoneNumber").value
-// console.log(name)
+function isId(){
+  if($('#userId').val()==""){
+    alert("email을 입력해주세요");
+    return false;
+  }else{
+    db.collection('customer').get().then((snapshot)=>{
+      snapshot.forEach((doc)=>{
+        if($('#userId').val() == doc.data()['email']){
+          alert("해당 email로 가입된 이력이 있습니다.");
+          return false;
+        }  
+      });
+    });
+  }
+  return true;
+}
 
-// const addRef = doc(db, 'hi', 'customer');
+function isPw(){
+  if($('#userPw').val() == ""){
+    alert("비밀번호를 입력해주세요.");
+    return false;
+  }
+  return true;
+}
 
-// // Set the "capital" field of the city 'DC'
-// await updateDoc(addRef, {
-//   test:'good'
-// });
+function equalPw(){
+  if($('#userPw2').val() == ""){
+    alert("비밀번호를 확인해주세요.");
+    return false;
+  }
+  if($('#userPw').val() != $('#userPw2').val()){
+    alert("비밀번호가 일치하지 않습니다.");
+    return false;
+  }
+  return true;
+}
+
+function isBirthday(){
+  if($('#userBirthDay').val() == ""){
+    alert("생년월일을 입력해주세요.");
+    return false;
+  }
+  if($('#userBirthDay').val().length != 6){
+    alert("생년월일을 6글자로 입력해주세요.");
+    return false;
+  }
+  if($.isNumeric($('#userBirthDay').val()) == false){
+    alert("숫자만 입력해주세요.");
+    return false;
+  }
+  return true;
+}
+
+function isPhoneNumber(){
+  if($('#phoneNumber').val() == ""){
+    alert("전화번호를 입력해주세요.");
+    return false;
+  }
+  if($('#phoneNumber').val().length != 11){
+    alert("전화번호를 11글자로 입력해주세요.");
+    return false;
+  }
+  if($.isNumeric($('#phoneNumber').val()) == false){
+    alert("숫자만 입력해주세요.");
+    return false;
+  }
+  return true;
+}
+
+$("#submit").click(function(){
+  if(isName() && isId() && isPw() && equalPw() && isBirthday() && isPhoneNumber()){
+    var name = $('#userName').val();
+    var id = $('#userId').val();
+    var pw = $('#userPw').val();
+    var birthday = $('#userBirthDay').val();
+    var phoneNumber = $('#phoneNumber').val();
+    var gender = $("input[name='userGender']:checked").val();
+    var area = $("#area option:selected").val();
+    var size;
+    db.collection('customer').get().then(snap=>{
+      size = Number(snap.doc.length);
+    });
+    console.log(size);
+    console.log(String(size));
+    db.collection('customer').doc(String(size)).set({name: name, id: id});
+    if($('#resume').val()){
+    
+    }
+    if($('#businessLicense').val()){
+
+    }
+  }else{
+    return false;
+  }
+});
