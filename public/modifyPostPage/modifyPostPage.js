e98edf11811b489488305c8da831195aa046f34c
esther7832 <esther7832@naver.com>
2021-12-04 21:38:52 +0900

    add userProfilePage

--- public/modifyPostPage/modifyPostPage.js
+++ public/modifyPostPage/modifyPostPage.js
@@ -15,3 +15,16 @@ function openSlideMenu(){
   });
 
 // 사이드바 끝
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCqJYyU3LacLWMFjix0SfgZt0Ajsuo5c-Q",
+    authDomain: "part-time-job-38ba6.firebaseapp.com",
+    projectId: "part-time-job-38ba6",
+    storageBucket: "part-time-job-38ba6.appspot.com",
+    messagingSenderId: "107342558639",
+    appId: "1:107342558639:web:f0e8e5c3845e6ed667eb5a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+const db = firebase.firestore();
