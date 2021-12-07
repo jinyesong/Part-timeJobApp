319dba9e98ae7c331a03cb5bb265e904e13cc2cd
GwakMirae <alfo9490@naver.com>
2021-12-07 20:22:17 +0900

    fix: literal error

--- public/bulletinBoard/jobOfferBoard.js
+++ public/bulletinBoard/jobOfferBoard.js
@@ -134,7 +134,7 @@ function sortAndFilter(){
         (periodList[period] == 3 && day > 7 && day <=30) | (periodList[period] == 4 && day > 30)){
           if((area == "No") | area == doc.data().area){
             console.log(postPay);
-            if(Number(pay) <= Number(postPay)){
+            if(pay <= postPay){
               var post = `<div class='post' id=${doc.id}>
               <label class='postTitle'>${title}</label>
               <label class='postWriter'><b>작성자</b> ${writer}</label>
@@ -142,9 +142,28 @@ function sortAndFilter(){
               <label class='postEnd'><b>마감일</b> ${postEnd}</label><br></div>
               </div>`
         $('#postList').append(post);
+        //마감전 시급인상 적용시 div배경색 바뀜
+        var payboost = doc.data().payboost;
+        // var increaseRate = doc.data().increaseRate;
+        var deadline = doc.data().deadline;
+        var postEnd = doc.data().postEnd;
+
+        if(payboost == "true"){
+          var today = new Date();
+          // var year = today.getFullYear();
+          // var month = ('0' + (today.getMonth() + 1)).slice(-2);
+          // var day = ('0' + today.getDate()).slice(-2);
+          // var dateString = year + '-' + month  + '-' + day;
+          var postEndDate = new Date(postEnd);
+          var boostStartDate = new Date(postEndDate.setDate(postEndDate.getDate() - deadline));
+          if((boostStartDate < today) || (boostStartDate == today)){
+            document.getElementById(doc.id).style.border = "2px solid red";
+          }
+        }
             } 
           }
         }
+        
       });
       
   });
@@ -180,6 +199,24 @@ function sortAndFilter(){
               <label class='postEnd'><b>마감일</b> ${postEnd}</label><br></div>
               </div>`
         $('#postList').append(post);
+        //마감전 시급인상 적용시 div배경색 바뀜
+        var payboost = doc.data().payboost;
+        // var increaseRate = doc.data().increaseRate;
+        var deadline = doc.data().deadline;
+        var postEnd = doc.data().postEnd;
+
+        if(payboost == "true"){
+          var today = new Date();
+          // var year = today.getFullYear();
+          // var month = ('0' + (today.getMonth() + 1)).slice(-2);
+          // var day = ('0' + today.getDate()).slice(-2);
+          // var dateString = year + '-' + month  + '-' + day;
+          var postEndDate = new Date(postEnd);
+          var boostStartDate = new Date(postEndDate.setDate(postEndDate.getDate() - deadline));
+          if((boostStartDate < today) || (boostStartDate == today)){
+            document.getElementById(doc.id).style.border = "2px solid red";
+          }
+        }
             } 
           }
         }
@@ -218,6 +255,24 @@ function sortAndFilter(){
               <label class='postEnd'><b>마감일</b> ${postEnd}</label><br></div>
               </div>`
         $('#postList').append(post);
+        //마감전 시급인상 적용시 div배경색 바뀜
+        var payboost = doc.data().payboost;
+        // var increaseRate = doc.data().increaseRate;
+        var deadline = doc.data().deadline;
+        var postEnd = doc.data().postEnd;
+
+        if(payboost == "true"){
+          var today = new Date();
+          // var year = today.getFullYear();
+          // var month = ('0' + (today.getMonth() + 1)).slice(-2);
+          // var day = ('0' + today.getDate()).slice(-2);
+          // var dateString = year + '-' + month  + '-' + day;
+          var postEndDate = new Date(postEnd);
+          var boostStartDate = new Date(postEndDate.setDate(postEndDate.getDate() - deadline));
+          if((boostStartDate < today) || (boostStartDate == today)){
+            document.getElementById(doc.id).style.border = "2px solid red";
+          }
+        }
             } 
           }
         }
