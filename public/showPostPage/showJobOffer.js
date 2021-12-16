022b9dbb4359d2dea1af146c3088254522ea27da
Yejin Song <67140032+jinyesong@users.noreply.github.com>
2021-12-17 00:26:29 +0900

    feat: Delete posts that are past the deadline

--- public/showPostPage/showJobOffer.js
+++ public/showPostPage/showJobOffer.js
@@ -29,7 +29,9 @@ $("#logoutBtn").click(function () {
 });
 
 $(".homeUserName").html(sessionStorage.getItem("name"));
-// 사이드바 끝 게시글 내용 출력
+// 사이드바 끝 
+
+//게시글 내용 출력
 var postId = sessionStorage.getItem("postId");
 
 var firebaseConfig = {
@@ -285,45 +287,3 @@ var map = new kakao.maps.Map(container, options);
 
 // 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
-
-// // 장소 검색 객체를 생성합니다
-// var ps = new kakao.maps.services.Places(); 
-
-// // 키워드로 장소를 검색합니다
- 
-
-// // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-// function placesSearchCB (data, status, pagination) {
-//     if (status === kakao.maps.services.Status.OK) {
-
-//         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-//         // LatLngBounds 객체에 좌표를 추가합니다
-//         var bounds = new kakao.maps.LatLngBounds();
-
-//         for (var i=0; i<data.length; i++) {
-//             displayMarker(data[i]);    
-//             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-//         }       
-
-//         // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-//         map.setBounds(bounds);
-//     } 
-// }
-
-// // 지도에 마커를 표시하는 함수입니다
-// function displayMarker(place) {
-    
-//     // 마커를 생성하고 지도에 표시합니다
-//     var marker = new kakao.maps.Marker({
-//         map: map,
-//         position: new kakao.maps.LatLng(place.y, place.x) 
-//     });
-
-    // // 마커에 클릭이벤트를 등록합니다
-    // kakao.maps.event.addListener(marker, 'click', function() {
-    //     var infowindow = new kakao.maps.InfoWindow();
-    //     // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-    //     infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
-    //     infowindow.open(map, marker);
-    // });
-//}
