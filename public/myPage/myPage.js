235d95a52709d35a1b603bd270826df73a0393ad
Yejin Song <67140032+jinyesong@users.noreply.github.com>
2021-12-17 10:37:11 +0900

    fix: delete comment

--- public/myPage/myPage.js
+++ public/myPage/myPage.js
@@ -1,8 +1,3 @@
-// var today = new Date();           var year = today.getFullYear();
-// var month = ('0' + (today.getMonth() + 1)).slice(-2);           var day =
-// ('0' + today.getDate()).slice(-2);           var dateString = year + '-' +
-// month  + '-' + day;           console.log(dateString);
-
 var firebaseConfig = {
   apiKey: "AIzaSyCqJYyU3LacLWMFjix0SfgZt0Ajsuo5c-Q",
   authDomain: "part-time-job-38ba6.firebaseapp.com",
